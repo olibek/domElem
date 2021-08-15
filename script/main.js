203c384848ceef44cb04f7e548ca40e45ac4a97e
Alibek Botashev <botashev_alibek@mail.ru>
2021-08-15 23:08:36 +0300

    feat(script.js): add new feat

--- script/main.js
+++ script/main.js
@@ -1,10 +1,11 @@
 'use strict';
-function DomElement(selector, height, width, bg, fontSize) {
+function DomElement(selector, height, width, bg, fontSize, position) {
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
+  this.position = position;
   this.create = function (text) {
     if (this.selector.charAt(0) === '.') {
       let elem = document.createElement('div');
@@ -15,8 +16,11 @@ function DomElement(selector, height, width, bg, fontSize) {
       width: ${this.width};
       background-color: ${this.bg};
       font-size: ${this.fontSize};
-    `;
-      elem.innerHTML = (text);
+      position: ${this.position};`;
+
+      if (text !== undefined) {
+        elem.innerHTML = (text);
+      }
       return elem;
     }
 
@@ -29,12 +33,45 @@ function DomElement(selector, height, width, bg, fontSize) {
       width: ${this.width};
       background-color: ${this.bg};
       font-size: ${this.fontSize};
-    `;
-      elem.innerHTML(text);
+      position: ${this.position};`;
+
+      if (text !== undefined) {
+        elem.innerHTML = (text);
+      }
       return elem;
     }
   };
 }
 
-let test = new DomElement('.dom', '400px', '500px', 'red', '24px');
-test.create('Привет мир');
+let test = new DomElement('#move', '100px', '100px', 'red', '24', 'absolute');
+// test.create('Привет');
+document.addEventListener('DOMContentLoaded', test.create('Привет'));
+
+const move = document.querySelector(test.selector);
+
+let moveLeft = 0;
+
+function direction(key) {
+  if (key.keyCode === 39) {
+    moveLeft += 10;
+    move.style.left = moveLeft + 'px';
+  }
+
+  if (key.keyCode === 37) {
+    moveLeft -= 10;
+    move.style.left = moveLeft + 'px';
+  }
+
+  if (key.keyCode === 38) {
+    moveLeft -= 10;
+    move.style.top = moveLeft + 'px';
+  }
+
+  if (key.keyCode === 40) {
+    moveLeft += 10;
+    move.style.top = moveLeft + 'px';
+  }
+}
+
+document.onkeydown = direction;
+
